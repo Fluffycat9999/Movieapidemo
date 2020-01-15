49f9768faf689202a7ea24da6b80be9df3af73d9
Fluffycat9999 <50632021+Fluffycat9999@users.noreply.github.com>
2020-01-15 11:43:51 -0500

    1-15-20(W)

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,17 @@
-//e-mail, name, graduated. require name and email. give graduated a default value = false
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(value) {
@@ -15,21 +20,16 @@ const userSchema = new mongoose.Schema({
       }
     }
   },
-  name: {
+  password: {
     type: String,
     required: true,
-    trim: true
-  },
-    password:{
-      type: String,
-      required: true,
-        trim: true,
-        minlength: 6,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error("Password can not include password")
-            }
-        }
+    trim: true,
+    minLength: 6,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot "password" ');
+      }
+    }
   },
   graduated: {
     type: Boolean,
@@ -37,13 +37,25 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-userSchema.pre('save', async function(next){
-    const user = this;
-    if(user.isModified('password')){
-        user.password = await bcrypt.hash(user.password, 7);
-    }
+userSchema.pre("save", async function(next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 7);
+  }
+  next();
 });
 //when we send a post or patch request, then bcrypt will run BEFORE he user password is saved to the mongo object
-const User = mongoose.model("user", {userSchema});
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({email});
+  if(!user) {
+    throw new Error('unable to login');
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch) {
+    throw new Error('incorrect password');
+  }
+  return user;
+};
+const User = mongoose.model("User", userSchema);
 module.exports = User;
