64d3a5140763c833c6e7a17ac1cb9c28e9536a2b
Fluffycat9999 <50632021+Fluffycat9999@users.noreply.github.com>
2020-01-28 23:30:26 -0500

    I tried

--- src/models/review.js
+++ src/models/review.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
-const Movie = mongoose.model('movie', {
-    title:{
-        type: String,
-        required: true
-    },
-    year:{
-        type: Number,
-        required: true
-    },
-    genre:{
-        type: String,
-        required: true
-    }
-});
+const Movie = require('../movie');
 
-module.exports = Movie;
+const reviewSchema = new mongoose.Schema({
+    review: { type: Number, min: 1, max: 5 }
+  });
+  const Review = db.model('Review', reviewSchema);
+  
+  const doc = new Review({ review: 'not a number' });
+  const err = doc.validateSync();
+  
+  err.errors['review'].name; // 'CastError'
+  // 'Cast to Number failed for value "not a number" at path "numWheels"'
+  err.errors['review'].message;
+  const Reviews = mongoose.model("Reviews", reviewSchema);
+  module.exports = Reviews;
--- src/routers/reviews.js
+++ src/routers/reviews.js
@@ -11,3 +11,23 @@ router.post('/movies', async (req, res) => {
         res.status(500).send(error);
     }
 });
+
+router.get('/movies/:id', async(req, res) => {
+    try{
+        let movie = await Movie.findById(req.params.id);
+        res.send(movie);
+    } catch(error){
+        res.status(500).send(error);
+    }
+});
+
+router.post('/movies/reviews/:id', async (req, res) => {
+    try{
+        const review = new Review(req.body);
+        const review = await Review.findById(req.params.id);
+        await review.save();
+        res.send(review);
+    } catch(error){
+        res.status(500).send(error);
+    }
+});
